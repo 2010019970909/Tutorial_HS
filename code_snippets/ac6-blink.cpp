6bd61d05ef0b8c46feeaf0454b8e073da4ea5d01
DomoGrid <vincent.stragier97@gmail.com>
2019-04-27 19:51:27 +0200

    Compile but 2 warnings on Overleaf

--- code_snippets/ac6-blink.cpp
+++ code_snippets/ac6-blink.cpp
@@ -63,44 +63,26 @@ static void MX_GPIO_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+int main(void)	{
+  /** Reset of all peripherals,
+    * Initializes the Flash interface 
+	* and the Systick. 
+	*/
+	
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
 	  HAL_GPIO_TogglePin (GPIOA, GPIO_PIN_5);
-	  HAL_Delay (2000); /* Insert delay 100 ms */
-    /* USER CODE BEGIN 3 */
+	  HAL_Delay (2000); /* Insert delay 2000 ms */
   }
-  /* USER CODE END 3 */
 }
 
 /**
